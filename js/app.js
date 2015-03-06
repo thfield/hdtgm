66d79d593599e810b266e39100ef04921f1b8d0b
thfield <thfield@gmail.com>
2015-03-05 18:11:34 -0800

    use cdn rather than local

--- js/app.js
+++ js/app.js
@@ -144,7 +144,6 @@ $(document).ready(function ( ) {
 		return catCounts;
 	}
 
-
 	function drawPie(data, cssTarget){
 		var width = 960,
 		    height = 500,
@@ -159,48 +158,61 @@ $(document).ready(function ( ) {
 
 		var pie = d3.layout.pie()
 		    .sort(null)
-		    .value(function(d) { return d.population; });
+		    .value(function(d) { return d.count; });
 
-		var svg = d3.select("body").append("svg")
+		var svg = d3.select(cssTarget).append("svg")
 		    .attr("width", width)
-		    .attr("height", height)
-		  .append("g")
-		    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+		    .attr("height", height);
 
-		d3.csv("data.csv", function(error, data) {
-
-		  data.forEach(function(d) {
-		    d.population = +d.population;
-		  });
-
-		  var g = svg.selectAll(".arc")
+		  var g = svg.append("g")
+		    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+		    .selectAll(".arc")
 		      .data(pie(data))
 		    .enter().append("g")
 		      .attr("class", "arc");
 
 		  g.append("path")
 		      .attr("d", arc)
-		      .style("fill", function(d) { return color(d.data.age); });
+		      .style("fill", function(d) { return color(d.data.category); });
 
 		  g.append("text")
 		      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
 		      .attr("dy", ".35em")
 		      .style("text-anchor", "middle")
-		      .text(function(d) { return d.data.age; });
+		      .text(function(d) { return d.data.count; });    
 
-		});
+		//var legend = d3.select("body").append("svg")
+		var legend = svg.append("g")
+	      .attr("class", "legend")
+	      .attr("width", radius * 2)
+	      .attr("height", radius * 2)
+	    .selectAll("g")
+	      .data(color.domain().slice().reverse())
+	    .enter().append("g")
+	      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+	  legend.append("rect")
+	      .attr("width", 18)
+	      .attr("height", 18)
+	      .style("fill", color);
+
+	  legend.append("text")
+	      .attr("x", 24)
+	      .attr("y", 9)
+	      .attr("dy", ".35em")
+	      .text(function(d) { return d; });
 	}
 
 	$.getJSON("data/list_actors.json", function(alldata) {
 	 	
-	 	console.log(countCategory(alldata, "rated"));
+	 	//console.log(countCategory(alldata, "rated"));
 
 		
 		drawCastCrew(countCrew(alldata, "actors"), ".drawActors");
-		/*
 		drawCastCrew(countCrew(alldata, "directors"), ".drawDirectors");
 		drawCastCrew(countCrew(alldata, "writers"), ".drawWriters");
-		*/
+		
+		drawPie(countCategory(alldata, "rated"), ".drawRatings");
 
 
 	});
