474373d45d3035400d2c899ed6ef0c32f9ff8451
thfield <thfield@gmail.com>
2015-10-22 20:54:42 -0700

    rewrite data gathering script

--- README.md
+++
@@ -1,3 +0,0 @@
-# hdtgm
-HDTGM data extravaganza
-"How Did This Get Made" is a podcast which talks about movies.  I'm using sources such as www.myapifilms.com and www.omdbapi.com to collect data about the movies they talk about.  Then I make visualizations with that data.
--- script/query.sh
+++ script/query.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cat get.txt | while read line; do 
+cat get.txt | while read line; do
     title=$(sed -e 's, ,+,g' <<< "$line")
-    #curl -s "http://www.omdbapi.com/?t=$title&y=&plot=short&r=json&tomatoes=true" >> temp_result.txt #search by title
+    # curl -s "http://www.omdbapi.com/?t=$title&y=&plot=short&r=json&tomatoes=true" >> temp_result.txt #search by title
     curl -s "http://www.omdbapi.com/?i=$title&y=&plot=short&r=json&tomatoes=true" >> temp_movies.txt #search by imdbID
-    curl -s "http://www.myapifilms.com/imdb?idIMDB=$title&actors=S" >> temp_actors.txt
+    # curl -s "http://www.myapifilms.com/imdb?idIMDB=$title&actors=S" >> temp_actors.txt
+    curl -s "http://www.myapifilms.com/imdb?idIMDB=$title&actors=F" >> temp_actors.txt
 
 done
